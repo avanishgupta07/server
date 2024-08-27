951cd6f5c68f1f1248876f58b17b665eac6013e7
avanishgupta07 <avanishguptaaza@gmail.com>
2024-08-27 17:35:59 +0530

    UPDATE

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,10 @@ import { uploadImage, getImage } from '../controller/image-controller.js';
 
 const router = express.Router();
 
+// Add a route for the root URL
+router.get('/', (req, res) => {
+    res.send('Welcome to the File Upload API');
+});
 
 router.post('/upload', upload.single('file'), uploadImage);
 router.get('/file/:fileId', getImage);
