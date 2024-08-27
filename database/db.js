960ecb06f77e1c07c6dc641c675d224a42d2851b
avanishgupta07 <avanishguptaaza@gmail.com>
2024-08-27 17:38:15 +0530

    UPDATE

--- database/db.js
+++ database/db.js
@@ -4,14 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const DBConnection = async () => {
-    // const USERNAME = process.env.DB_USERNAME;
-    // const PASSWORD = process.env.DB_PASSWORD;
-
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log('Error while connecting with the database ', error.message);
